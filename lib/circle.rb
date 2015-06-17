c7bdc6e0242790c012d07995fdafd3d5989052ad
Mark Scheel <mscheel@itriagehealth.com>
2015-06-17 07:46:20 -0600

    first homework, learning ruby!

--- lib/circle.rb
+++ lib/circle.rb
@@ -5,7 +5,31 @@
 
 class Circle
 
-	# your code here
+	# set up instance variable, get getter and setter for free
+	attr_accessor :radius
+	
+	# set up constructor
+	def initialize radius
+		self.radius = radius
+	end 
+
+	def radius
+		# @ sign signifies that is an instance variable, so as not to confuse
+		# with the method that is also self.radius
+		
+		# return is implied for last statement of anything, so can omit
+		# return @radius would also work
+		@radius
+	end
+
+	# expect(circle.area).to eq(Math::PI * (rad ^ 2))
+	def area
+		Math::PI * (self.radius ^ 2)
+	end
+
+	def circumference
+		2 * Math::PI * self.radius
+	end
 
 	# def draw
 	# 	img = Image.new(radius*2+100,radius*2+100) { self.background_color = "white" }
